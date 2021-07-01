ef4c2d04c3b11e50874147ff15ffc18ec348aeb4
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-07-02 00:46:04 +0200

    v0.7.0

--- examples/basic.rs
+++ examples/basic.rs
@@ -11,7 +11,7 @@ struct MathOp {
 }
 
 fn main() {
-  let platform = v8::new_default_platform().unwrap();
+  let platform = v8::new_default_platform(0, false).make_shared();
   v8::V8::initialize_platform(platform);
   v8::V8::initialize();
 
