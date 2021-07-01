f9f907fe2b6b3c164da91682c9ab417b66aaea02
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-07-01 23:13:38 +0200

    v0.7.0

--- src/utils.rs
+++ src/utils.rs
@@ -12,7 +12,7 @@ pub fn js_exec<'s>(
 }
 
 pub fn v8_init() {
-  let platform = v8::new_default_platform().unwrap();
+  let platform = v8::new_default_platform(0, false).make_shared();
   v8::V8::initialize_platform(platform);
   v8::V8::initialize();
 }
